a2b0aa9d9bc62358c720a91268103f989bacf12f
mbuhah-gh <tan.d20@mhs.istts.ac.id>
2021-11-28 18:45:16 +0700

    pindah Intent login register

--- app/src/main/java/id/ac/cookbook/Login.java
+++ app/src/main/java/id/ac/cookbook/Login.java
@@ -5,17 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class Register extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
     }
 
     @Override
@@ -27,18 +26,18 @@ public class Register extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.option_logreg_home){
-            Intent toHome = new Intent(Register.this, MainActivity.class);
+            Intent toHome = new Intent(Login.this, MainActivity.class);
             startActivity(toHome);
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public void registerClick(View v){
-        if (v.getId() == R.id.btnRegister){
+    public void loginClick(View v){
+        if (v.getId() == R.id.btnLogin){
 
-        }else if (v.getId() == R.id.btnRegisterLogin){
-            Intent toLogin = new Intent(Register.this, Login.class);
-            startActivity(toLogin);
+        }else if (v.getId() == R.id.btnLoginRegister){
+            Intent toRegister = new Intent(Login.this, Register.class);
+            startActivity(toRegister);
         }
     }
 }
--- app/src/main/java/id/ac/cookbook/MainActivity.java
+++ app/src/main/java/id/ac/cookbook/MainActivity.java
@@ -1,8 +1,12 @@
 package id.ac.cookbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (getIntent().hasExtra("user")){
+            getMenuInflater().inflate(R.menu.option_user, menu);
+        }else{
+            getMenuInflater().inflate(R.menu.option_anonymous, menu);
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (getIntent().hasExtra("user")){
+
+        }else{
+            if (item.getItemId() == R.id.option_login){
+                Intent toLogin = new Intent(MainActivity.this, Login.class);
+                startActivity(toLogin);
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
